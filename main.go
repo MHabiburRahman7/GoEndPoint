6ae444a60c02f259ac06d4d9a978422129d7ea1f
rahman <george_the@ymail.com>
2022-02-21 21:35:43 +0900

    stuck at mux

--- main.go
+++ main.go
@@ -4,7 +4,21 @@ import (
     "fmt"
     "log"
     "net/http"
+    "encoding/json"
 )
+
+type ArticleData struct{
+  Title string `json:"Title"`
+  Desc string `json:"Description"`
+  Content string `json:"Content"`
+}
+
+var ArticleDatas []ArticleData
+
+func returnAllArticles(w http.ResponseWriter, r *http.Request){
+  fmt.Println("Well, hit again")
+  json.NewEncoder(w).Encode(ArticleDatas)
+}
 
 func homePage(w http.ResponseWriter, r *http.Request){
     fmt.Fprintf(w, "Welcome to the HomePage!")
@@ -12,10 +26,32 @@ func homePage(w http.ResponseWriter, r *http.Request){
 }
 
 func handleRequests() {
+    http.HandleFunc("/articles", returnAllArticles)
     http.HandleFunc("/", homePage)
     log.Fatal(http.ListenAndServe(":10000", nil))
 }
 
+func handleRequestsUsingMux(){
+    // creates a new instance of a mux router
+    myRouter := mux.NewRouter().StrictSlash(true)
+    // replace http.HandleFunc with myRouter.HandleFunc
+    myRouter.HandleFunc("/", homePage)
+    myRouter.HandleFunc("/all", returnAllArticles)
+    // finally, instead of passing in nil, we want
+    // to pass in our newly created router as the second
+    // argument
+    log.Fatal(http.ListenAndServe(":10000", myRouter))
+}
+
+func initArticles(){
+  ArticleDatas = []ArticleData{
+        ArticleData{Title: "Hello World", Desc: "Article Description 1", Content: "Content"},
+        ArticleData{Title: "Hello Again", Desc: "Article Description 2", Content: "Content"},
+    }
+}
+
 func main() {
-    handleRequests()
+    initArticles()
+    //handleRequests()
+    handleRequestsUsingMux()
 }
